93629531a3458de9332fbfc9c0b960085b194317
Bk <bikaleg@gmail.com>
2020-06-21 17:34:58 -0500

    integrate http request

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,58 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import Header from "./components/layout/Header";
 export default {
   name: "App",
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo,
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+      todos: [],
     };
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id != id);
-    }
-  }
+      fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: "DELETE",
+      })
+        .then(() => {
+          this.todos = this.todos.filter((todo) => todo.id != id);
+        })
+        .catch(console.log);
+    },
+    addTodo(newTodo) {
+      fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTodo),
+      })
+        .then((res) => res.json())
+        .then((data) => (this.todos = [...this.todos, data]))
+        .catch(console.log);
+    },
+  },
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=7")
+      .then((res) => res.json())
+      .then((data) => {
+        this.todos = data;
+      })
+      .catch(console.log);
+  },
 };
 </script>
 
