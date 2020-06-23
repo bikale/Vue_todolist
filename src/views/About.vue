0de47cb81313b1f5cec98482a3ce23e2496c4a4d
Bk <bikaleg@gmail.com>
2020-06-22 20:42:14 -0500

    added routing

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>
+      Lorem ipsum dolor sit amet,
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsam, omnis
+      eos aspernatur laborum facilis neque. Corrupti, exercitationem, doloribus
+      sint nulla pariatur assumenda cumque distinctio tenetur praesentium
+      dolorum, consectetur voluptates?
+    </p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from "../components/Todos";
+import AddTodo from "../components/AddTodo";
+import Header from "../components/layout/Header";
+export default {
+  name: "Home",
+  components: {
+    Todos,
+    Header,
+    AddTodo,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: "DELETE",
+      })
+        .then(() => {
+          this.todos = this.todos.filter((todo) => todo.id != id);
+        })
+        .catch(console.log);
+    },
+    addTodo(newTodo) {
+      fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTodo),
+      })
+        .then((res) => res.json())
+        .then((data) => (this.todos = [...this.todos, data]))
+        .catch(console.log);
+    },
+  },
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=7")
+      .then((res) => res.json())
+      .then((data) => {
+        this.todos = data;
+      })
+      .catch(console.log);
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+</style>
